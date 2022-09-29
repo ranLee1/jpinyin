4d04db39f8dc0a843c3a1ae93ca79f12951e6006
bo peng <pengbd3@163.com>
2022-09-29 14:37:03 +0800

    Initial commit

---
+++ src/main/java/com/github/ranlee1/jpinyin/PinyinException.java
@@ -0,0 +1,15 @@
+package com.github.ranlee1.jpinyin;
+
+/**
+ * 异常类
+ *
+ */
+public class PinyinException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public PinyinException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/com/github/ranlee1/jpinyin/PinyinFormat.java
@@ -0,0 +1,9 @@
+package com.github.ranlee1.jpinyin;
+
+/**
+ * 拼音格式类
+ *
+ */
+public enum PinyinFormat {
+    WITH_TONE_MARK, WITHOUT_TONE, WITH_TONE_NUMBER;
+}
